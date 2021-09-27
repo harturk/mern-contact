4db2d36bcafc7c5b5bd376105b2dece43593761e
Hártur Mainardi <hartur.kunzler@gmail.com>
2021-09-27 05:19:40 -0300

    Configuração requisições api

--- server.js
+++ server.js
@@ -4,7 +4,11 @@ const app = express();
 
 app.get('/', (req, res) => res.json({ msg: "Welcome to the ContactKeepr API..." }));
 
+// Define routes
+app.use('/api/users', require('./routes/users'));
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/contacts', require('./routes/contacts'));
+
 const PORT = process.env.POST || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
-
