da06ff275e0a2847201514ec3d585a2d329f40ce
Hartur Mainardi <hartur.kunzler@gmail.com>
2021-09-28 13:43:43 -0300

    Config server com routes

--- server.js
+++ server.js
@@ -1,13 +1,19 @@
-const express = require('express');
+const express = require("express");
+const connectDB = require("./config/db");
 
 const app = express();
 
-app.get('/', (req, res) => res.json({ msg: "Welcome to the ContactKeepr API..." }));
+// Connect Database
+connectDB();
+
+app.get("/", (req, res) =>
+  res.json({ msg: "Welcome to the ContactKeepr API..." })
+);
 
 // Define routes
-app.use('/api/users', require('./routes/users'));
-app.use('/api/auth', require('./routes/auth'));
-app.use('/api/contacts', require('./routes/contacts'));
+app.use("/api/users", require("./routes/users"));
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/contacts", require("./routes/contacts"));
 
 const PORT = process.env.POST || 5000;
 
